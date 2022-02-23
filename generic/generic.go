f410f2daaf47441d78cd6245f0cbf03da59d629f
Tabitha <tabitha@keyneston.com>
2022-02-23 21:38:25 +0100

    First bit of work to make a generic oauth2 provider

---
+++ generic/generic.go
@@ -0,0 +1,137 @@
+// Package discord provides you access to Discord's OAuth2
+// infrastructure.
+package discord
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+type Endpoint struct {
+	conf  *oauth2.Config
+	state string // TODO: replace this with a signed token
+	store sessions.CookieStore
+}
+
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  url.URL
+	Endpoint     oauth2.Endpoint
+	Scopes       []string
+}
+
+func randToken() string {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		glog.Fatalf("[Gin-OAuth] Failed to read rand: %v\n", err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Setup(config Config) *Endpoint {
+	store := sessions.NewCookieStore([]byte(config.ClientSecret))
+	conf := &oauth2.Config{
+		ClientID:     config.ClientID,
+		ClientSecret: config.ClientSecret,
+		RedirectURL:  config.RedirectURL.String(),
+		Scopes:       config.Scopes,
+		Endpoint:     config.Endpoint,
+	}
+
+	return &Endpoint{
+		conf:  conf,
+		store: store,
+	}
+}
+
+func (e *Endpoint) Session(name string) gin.HandlerFunc {
+	return sessions.Sessions(name, e.store)
+}
+
+func (e *Endpoint) LoginHandler(ctx *gin.Context) {
+	state := randToken()
+	session := sessions.Default(ctx)
+	session.Set("state", state)
+	session.Save()
+	ctx.Writer.Write([]byte("<html><title>Golang Github</title> <body> <a href='" + e.GetLoginURL(state) + "'><button>Login with GitHub!</button> </a> </body></html>"))
+}
+
+func (e *Endpoint) GetLoginURL(state string) string {
+	return e.conf.AuthCodeURL(state)
+}
+
+type AuthUser struct {
+	Login   string `json:"login"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Company string `json:"company"`
+	URL     string `json:"url"`
+}
+
+func init() {
+	gob.Register(AuthUser{})
+}
+
+func (e *Endpoint) Auth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var (
+			ok       bool
+			authUser AuthUser
+			user     *github.User
+		)
+
+		// Handle the exchange code to initiate a transport.
+		session := sessions.Default(ctx)
+		mysession := session.Get("ginoauthgh")
+		if authUser, ok = mysession.(AuthUser); ok {
+			ctx.Set("user", authUser)
+			ctx.Next()
+			return
+		}
+
+		retrievedState := session.Get("state")
+		if retrievedState != ctx.Query("state") {
+			ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+			return
+		}
+
+		// TODO: oauth2.NoContext -> context.Context from stdlib
+		tok, err := e.conf.Exchange(oauth2.NoContext, ctx.Query("code"))
+		if err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("Failed to do exchange: %v", err))
+			return
+		}
+		client := github.NewClient(e.conf.Client(oauth2.NoContext, tok))
+		user, _, err = client.Users.Get(oauth2.NoContext, "")
+		if err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("Failed to get user: %v", err))
+			return
+		}
+
+		// save userinfo, which could be used in Handlers
+		authUser = AuthUser{
+			Login: *user.Login,
+			Name:  *user.Name,
+			URL:   *user.URL,
+		}
+		ctx.Set("user", authUser)
+
+		// populate cookie
+		session.Set("ginoauthgh", authUser)
+		if err := session.Save(); err != nil {
+			glog.Errorf("Failed to save session: %v", err)
+		}
+	}
+}
